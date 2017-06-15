070c508f59ed514c79f543a720a43dfa5b9dc513
Jason Skipper <jason.skipper@theironyard.com>
2017-06-15 08:04:07 -0400

    Added Paging and Sorting to ToDoController

--- src/main/java/com/ironyard/springboot/controller/ToDoController.java
+++ src/main/java/com/ironyard/springboot/controller/ToDoController.java
@@ -4,10 +4,14 @@ import java.util.Collection;
 import java.util.HashMap;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ironyard.repo.NotePadRepository;
@@ -53,8 +57,25 @@ public class ToDoController {
 	 * @return Collection of all ToDoItems
 	 */
 	@RequestMapping(value = "/todo", method = RequestMethod.GET)
-	public Collection<ToDoItem> get() {
-		// set ID
-		return myToDoRepo.findAll();
+	public Page get(@RequestParam("page") Integer page,
+            @RequestParam("size") Integer size,
+            @RequestParam(value = "sortby", required = false) String sortby,
+            @RequestParam(value = "dir", required = false) Sort.Direction direction) {
+
+
+		// DEFAULT Sort property
+		if (sortby == null) {
+			sortby = "title";
+		}
+		
+		// DEFAULT Sort direction
+		if (direction == null) {
+			direction = Sort.Direction.DESC;
+		}
+		
+		Sort s = new Sort(direction, sortby);
+		PageRequest pr = new PageRequest(page, size, s);
+		Page data =  myToDoRepo.findAll(pr);		
+		return data;
 	}
 }
