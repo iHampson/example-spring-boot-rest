06a82aece54f65e206c822668f37a48e3b969e5b
iHampson <imh8602@rit.edu>
2017-06-16 11:44:43 -0400

    Adding in the repo files for our project

---
+++ src/main/java/com/ironyard/repo/PetRepo.java
@@ -0,0 +1,9 @@
+package com.ironyard.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ironyard.springboot.data.Pet;
+
+public interface PetRepo extends JpaRepository<Pet, Long>{
+
+}
