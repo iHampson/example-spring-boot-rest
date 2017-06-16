770e351caef689eb8043d494ccdda0d40de9d7b9
iHampson <imh8602@rit.edu>
2017-06-16 11:51:18 -0400

    Update Pet Repo Interface

--- src/main/java/com/ironyard/repo/PetRepo.java
+++ src/main/java/com/ironyard/repo/PetRepo.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.ironyard.springboot.data.Pet;
 
 public interface PetRepo extends JpaRepository<Pet, Long>{
-
+  List<Pet> findByBreed(String breed);
 }
